2f8116f2430075e4c1ff7d3a35aa9ec524bdc7f3
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-04-13 09:47:25 +0200

    Initial commit modal

---
+++ Accommodation.php
@@ -0,0 +1,37 @@
+<?php
+
+class Accommodation
+{
+	private static $instance;
+	public $db;
+	public $singular;
+	public $path;
+	public $plural;
+	public $customFields;
+	public $nameSpace;
+	public $icon;
+
+	public function __construct($singular, $plural, $path, $customFields)
+	{
+		$this->singular = $singular;
+		$this->plural = $plural;
+		$this->db = 'bs-course';
+		$this->nameSpace = 'bs_course';
+		$this->path = $path;
+		$this->icon = 'dashicons-store';
+		$this->customFields = $customFields;
+	}
+
+	public static function getInstance($singular, $plural, $path, $customFields)
+	{
+		if (!self::$instance instanceof self) {
+			self::$instance = new self($singular, $plural, $path, $customFields);
+		}
+		return self::$instance;
+	}
+
+}
+
+
+
+
